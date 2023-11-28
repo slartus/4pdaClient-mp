766c032a30d6eebdb00a279926137479aa658b57
Artem Slinkin <slartus@gmail.com>
2023-11-28 22:57:34 +0500

    ios run fixed

--- shared/app/src/iosMain/kotlin/main.ios.kt
+++ shared/app/src/iosMain/kotlin/main.ios.kt
@@ -1,5 +1,6 @@
 import androidx.compose.ui.window.ComposeUIViewController
 
 import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
 
-fun MainViewController() = ComposeUIViewController { Text("test") }
+fun MainViewController() = ComposeUIViewController { Text("test1123123123", color = Color.Black) }
