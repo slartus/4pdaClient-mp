6ae5f7569a7691f5ce1a1df2f5e504f081e1159e
Artem Slinkin <slartus@gmail.com>
2023-11-28 19:47:09 +0500

    drawer

--- shared/app/src/commonMain/kotlin/ru/slartus/forpda/App.kt
+++ shared/app/src/commonMain/kotlin/ru/slartus/forpda/App.kt
@@ -1,27 +1,62 @@
 package ru.slartus.forpda
 
 import RootComponent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Button
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.Children
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.fade
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.plus
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.scale
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.stackAnimation
+import kotlinx.coroutines.launch
 import ru.slartus.forpda.news_list.compose.NewsList
 
 @Composable
 fun App(rootComponent: RootComponent) {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+
     MaterialTheme {
-        Children(
-            stack = rootComponent.stack,
-            modifier = Modifier.fillMaxSize(),
-            animation = stackAnimation(fade() + scale()),
+        ModalNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                ModalDrawerSheet {
+                    Text("HELLO")
+                }
+            },
         ) {
-            when (val child = it.instance) {
-                is RootComponent.Child.NewsListChild -> NewsList(component = child.component)
+            Column {
+                Button(
+                    onClick = {
+                        scope.launch {
+                            drawerState.apply {
+                                if (isClosed) open() else close()
+                            }
+                        }
+                    }
+                ){
+                    Text("click me")
+                }
+                Children(
+                    stack = rootComponent.stack,
+                    modifier = Modifier.fillMaxSize(),
+                    animation = stackAnimation(fade() + scale()),
+                ) {
+                    when (val child = it.instance) {
+                        is RootComponent.Child.NewsListChild -> NewsList(component = child.component)
+                    }
+                }
             }
         }
     }
